2f66f50e12a6f096452955ce2b1a3751beca2534
nikharjain16 <Nikharjain27@gmail.com>
2023-04-09 18:27:25 +0530

    Ui Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/AppComponent', pathMatch: 'full' },
+  { path: 'product', component: ProductsComponent  },
+  { path: 'home',  component: HomeComponent},
+  { path: 'login',  component: LoginComponent}
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
