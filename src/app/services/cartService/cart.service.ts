35d7c9d78fb560d210273038a53c57d87cea6d50
Yogesh Manatkar <yogeshmanatkar137@gmail.com>
2023-04-18 14:42:20 +0530

    add vedraj file

--- src/app/services/cartService/cart.service.ts
+++ src/app/services/cartService/cart.service.ts
@@ -1,42 +1,65 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const baseUrl = "http://localhost:9091/cart-section/";
+const baseUrl = 'http://localhost:9091/cart-section/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  customerEmail = localStorage.getItem('customerEmailId');
+
+  token = localStorage.getItem('authenticationToken');
 
   getCart(cartId: number): Observable<any> {
-
-     return this.http.get(`${baseUrl}/${cartId}`);
-
+    return this.http.get(`${baseUrl}/${cartId}`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      }),
+    });
   }
   addToCart(cartId: number, productId: number): Observable<any> {
+    return this.http.post(
+      `${baseUrl}/add-to-cart/${cartId}/${productId}`,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.token}`,
+        }),
+      }
+    );
+  }
 
-   return this.http.post(`${baseUrl}/add-to-cart/${cartId}/${productId}`, null);
-
-   }
-
-
-   increaseQuantity(cartId: number, cartItemId: number): Observable<any> {
-    return this.http.post(`${baseUrl}/${cartId}/${cartItemId}`, null);
+  increaseQuantity(cartId: number, cartItemId: number): Observable<any> {
+    return this.http.post(`${baseUrl}/${cartId}/${cartItemId}`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      }),
+    });
   }
 
   removeFromCart(cartId: number, cartItemId: number): Observable<any> {
-    return this.http.delete(`${baseUrl}/${cartId}/${cartItemId}`);
+    return this.http.delete(`${baseUrl}/${cartId}/${cartItemId}`,
+    {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      }),
+    });
   }
 
   removeAllFromCart(cartId: number): Observable<any> {
-    return this.http.delete(`${baseUrl}/${cartId}`);
+    return this.http.delete(`${baseUrl}/${cartId}` ,
+    {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      }),
+    });
   }
-
-
-
-
-
 }
